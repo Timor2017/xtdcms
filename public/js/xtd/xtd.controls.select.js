8c4c121f9cd90ca5e415ad1393584236ece00665
zhangyanan <ivyzhang_happy@163.com>
2016-12-08 16:53:49 +0800

    fix select control's name

--- public/js/xtd/xtd.controls.select.js
+++ public/js/xtd/xtd.controls.select.js
@@ -50,13 +50,13 @@ try {
 				$('#lbl_'+$(this).attr('data-parent-id')).html(value);
 			}));
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.common.default_value).setParent(this).subscribe(function (value) {
-				$('#txt_'+$(this).attr('data-parent-id')).val(value);
+				$('#select_'+$(this).attr('data-parent-id')).val(value);
 			}));
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.common.placeholder).setParent(this).subscribe(function (value) {
-				$('#txt_'+$(this).attr('data-parent-id')).attr('placeholder', value);
+				$('#select_'+$(this).attr('data-parent-id')).attr('placeholder', value);
 			}));
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.common.tooltips).setParent(this).subscribe(function (value) {
-				$('#txt_'+$(this).attr('data-parent-id')).attr('title', value);
+				$('#select_'+$(this).attr('data-parent-id')).attr('title', value);
 			}));
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.common.is_searchable).setParent(this));
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.common.is_show_in_list).setParent(this));
@@ -64,49 +64,49 @@ try {
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.common.sort_sequence).setParent(this));
 			
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.layout.width).setParent(this).subscribe(function (value) {
-				$('#txt_'+$(this).attr('data-parent-id')).css('width', value);
+				$('#select_'+$(this).attr('data-parent-id')).css('width', value);
 			}));
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.layout.height).setParent(this).subscribe(function (value) {
-				$('#txt_'+$(this).attr('data-parent-id')).css('height', value);
+				$('#select_'+$(this).attr('data-parent-id')).css('height', value);
 			}));
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.layout.horizontalAlignment).setParent(this).subscribe(function (value) {
-				$('#txt_'+$(this).attr('data-parent-id')).css('text-aign', value);
+				$('#select_'+$(this).attr('data-parent-id')).css('text-aign', value);
 			}));
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.layout.verticalAlignment).setParent(this).subscribe(function (value) {
-				$('#txt_'+$(this).attr('data-parent-id')).css('vertical-align', value);
+				$('#select_'+$(this).attr('data-parent-id')).css('vertical-align', value);
 			}));
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.layout.marginTop).setParent(this).subscribe(function (value) {
-				$('#txt_'+$(this).attr('data-parent-id')).css('margin-top', value);
+				$('#select_'+$(this).attr('data-parent-id')).css('margin-top', value);
 			}));
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.layout.marginRight).setParent(this).subscribe(function (value) {
-				$('#txt_'+$(this).attr('data-parent-id')).css('margin-right', value);
+				$('#select_'+$(this).attr('data-parent-id')).css('margin-right', value);
 			}));
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.layout.marginBottom).setParent(this).subscribe(function (value) {
-				$('#txt_'+$(this).attr('data-parent-id')).css('margin-bottom', value);
+				$('#select_'+$(this).attr('data-parent-id')).css('margin-bottom', value);
 			}));
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.layout.marginLeft).setParent(this).subscribe(function (value) {
-				$('#txt_'+$(this).attr('data-parent-id')).css('margin-left', value);
+				$('#select_'+$(this).attr('data-parent-id')).css('margin-left', value);
 			}));
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.brush.backgroundColor).setParent(this).subscribe(function (value) {
-				$('#txt_'+$(this).attr('data-parent-id')).css('background-color', value);
+				$('#select_'+$(this).attr('data-parent-id')).css('background-color', value);
 			}));
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.brush.backgroundImage).setParent(this).subscribe(function (value) {
-				$('#txt_'+$(this).attr('data-parent-id')).css('background-image', value);
+				$('#select_'+$(this).attr('data-parent-id')).css('background-image', value);
 			}));
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.brush.foregroundColor).setParent(this).subscribe(function (value) {
-				$('#txt_'+$(this).attr('data-parent-id')).css('color', value);
+				$('#select_'+$(this).attr('data-parent-id')).css('color', value);
 			}));
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.text.size).setParent(this).subscribe(function (value) {
-				$('#txt_'+$(this).attr('data-parent-id')).css('font-size', value);
+				$('#select_'+$(this).attr('data-parent-id')).css('font-size', value);
 			}));
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.text.weight).setParent(this).subscribe(function (value) {
-				$('#txt_'+$(this).attr('data-parent-id')).css('font-weight', value);
+				$('#select_'+$(this).attr('data-parent-id')).css('font-weight', value);
 			}));
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.text.textDecoration).setParent(this).subscribe(function (value) {
-				$('#txt_'+$(this).attr('data-parent-id')).css('text-decoration', value);
+				$('#select_'+$(this).attr('data-parent-id')).css('text-decoration', value);
 			}));
 			this.properties.add(XTD.factories.PropertyFactory.generate(this.definition.properties.text.style).setParent(this).subscribe(function (value) {
-				$('#txt_'+$(this).attr('data-parent-id')).css('font-style', value);
+				$('#select_'+$(this).attr('data-parent-id')).css('font-style', value);
 			}));
 					
 		};
